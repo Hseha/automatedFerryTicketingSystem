31e793e07667e3c0e8aadccd655c9f7b3340cfe5
hseha <vallesjohnmichael32@gmail.com>
2026-04-17 02:07:04 +0800

    added gitignore

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mvn compile exec:java -Dexec.mainClass="com.mycompany.automatedferryticketingsystem.view.WelcomeScreen" \
-                      -DDB_URL=jdbc:mariadb://127.0.0.1:3306/ferry_ticketing_db \
-                      -DDB_USER=ferry_user \
-                      -DDB_PASS=michaelvalles123
